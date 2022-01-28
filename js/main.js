a6358f70ab48ec03fedc109baaaf2ae038614138
unknown <jasurbekturgunov25@gmail.com>
2022-01-28 23:44:51 +0500

    tayyor

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+// var toggle = document.getElementById('togle')
+
+var toggle = document.querySelector('.navbar__togle')
+var navbar = document.querySelector('.navbar__sitebar')
+toggle. addEventListener('click',()=>{
+
+     if(toggle.classList.contains('navbar__togle--active')){
+        navbar.classList.remove('navbar__sitebar--active')
+        toggle.classList.remove('navbar__togle--active')
+
+     }else{
+        toggle.classList.add('navbar__togle--active')
+        navbar.classList.add('navbar__sitebar--active')
+
+     }
+    
+    
+
+})
+
