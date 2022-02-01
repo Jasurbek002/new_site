ca3af0be53e039d84c03d212784bfe0ef44b7910
unknown <jasurbekturgunov25@gmail.com>
2022-02-01 22:52:20 +0500

    tayyor

--- js/main.js
+++ js/main.js
@@ -21,10 +21,48 @@ toggle. addEventListener('click',()=>{
 })
 
 var descimg = document.querySelector('.glavniimg')
-var btnimg = document.querySelector('.maskimg')
+var btnimg = document.getElementsByClassName('maskimg')
 
-btnimg.addEventListener('click',()=>{
-   descimg.src=btnimg.src
+for(let i=0;i<btnimg.length;i++){
+   btnimg[i].addEventListener("click",(evt)=>{
+      // console.log(evt.target.src)
+      descimg.src = evt.target.src
+   })
+}
+
+var star = document.getElementsByClassName('stars')
+
+for(let i = 0;i<star.length;i++){
+   star[i].addEventListener('click',(button)=>{
+      button.target.style.background="black"
+   })
+}
+
+var plusel = document.getElementById('plus')
+ var   resaltel= document.getElementById('resalt')
+  var  minusel =document.getElementById('minus')
+
+
+plusel.addEventListener("submit",()=>{
+  
+   resaltel.textcontent + 1
+  
 })
 
+var korzinkabar = document.querySelector('.imgkor')
+var sec = document.querySelector('.hearor')
+korzinkabar.style.background='red'
 
+korzinkabar.addEventListener('click',() =>{
+
+      var card = document.createElement('div')
+      card.style.position='absolute'
+      card.style.right='0px'
+      card.style.width="300px"
+      card.style.height='600px'
+      card.textContent="hello"
+      card.style.background='red'
+   console.log(card)
+  
+  sec.appendChild(card)
+})
